ac5232238e5df1c5c7e1ef14f99e49c2b34d7b8f
Sharan <sharanpyata1808@gmail.com>
2024-01-14 19:10:45 -0600

    Terraform module updates

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -3,10 +3,35 @@ resource "aws_vpc" "main" {
   tags = merge(var.tags, { Name = var.env})
 }
 
+## This subnet blocks needs improvement in terms of Code Dry
 resource "aws_subnet" "public" {
   count      = length(var.public_subnets)
   vpc_id     = aws_vpc.main.id
   cidr_block = var.public_subnets[count.index]
   tags       = merge(var.tags, { Name = "public-subnet" })
+  availability_zone = var.azs[count.index]
+}
+
+resource "aws_subnet" "web" {
+  count      = length(var.web_subnets)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.web_subnets[count.index]
+  tags       = merge(var.tags, { Name = "web-subnet" })
+  availability_zone = var.azs[count.index]
+}
+
+resource "aws_subnet" "app" {
+  count      = length(var.app_subnets)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.app_subnets[count.index]
+  tags       = merge(var.tags, { Name = "app-subnet" })
+  availability_zone = var.azs[count.index]
+}
+
+resource "aws_subnet" "db" {
+  count      = length(var.db_subnets)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.db_subnets[count.index]
+  tags       = merge(var.tags, { Name = "db-subnet" })
   availability_zone = var.azs[count.index]
 }
